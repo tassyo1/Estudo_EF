16ad342dd01cb6ce952b18ab515cdffddf0bb097
tassyo.santana <tassyo.santana@hotmail.com>
2016-06-24 18:25:16 -0300

    Fim da aula1

---
+++ Estudo_EF/EntidadesContext.cs
@@ -0,0 +1,15 @@
+﻿using Estudo_EF.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudo_EF
+{
+    class EntidadesContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
--- Estudo_EF/Program.cs
+++ Estudo_EF/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Estudo_EF.Entidades;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,14 @@ namespace Estudo_EF
     {
         static void Main(string[] args)
         {
+            EntidadesContext contexto = new EntidadesContext();
+            contexto.Database.CreateIfNotExists();
+
+            Usuario usuario = new Usuario() { Nome = "Tássyo", Senha = "123" };
+            contexto.Usuarios.Add(usuario);
+
+            contexto.SaveChanges();
+            contexto.Dispose();
         }
     }
 }
