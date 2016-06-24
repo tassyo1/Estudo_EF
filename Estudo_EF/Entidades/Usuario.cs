cd44430613caf50e0eca65103763a0783954eecd
tassyo.santana <tassyo.santana@hotmail.com>
2016-06-24 17:49:56 -0300

    Inicial - Aula 1 EF

---
+++ Estudo_EF/Entidades/Usuario.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Estudo_EF.Entidades
+{
+    public class Usuario
+    {
+        public int ID { get; set; }
+        public string Nome { get; set; }
+        public string Senha { get; set; }
+    }
+}
